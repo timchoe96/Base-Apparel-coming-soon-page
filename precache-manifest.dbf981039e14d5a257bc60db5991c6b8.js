2ba692ec2dba1b1a27dad247fb0fb8c868b88738
Timothy Choe <timothychoe@MacBook-Pro.local>
2020-06-21 01:37:49 -0400

    Updates

---
+++ precache-manifest.dbf981039e14d5a257bc60db5991c6b8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870ce03266d1b2c8ccacb53f18c93c68",
+    "url": "/Base-Apparel-coming-soon-page/index.html"
+  },
+  {
+    "revision": "34f1dba56424e05a86eb",
+    "url": "/Base-Apparel-coming-soon-page/static/js/2.e415505a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Base-Apparel-coming-soon-page/static/js/2.e415505a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea630a2d0e2de78f0e11",
+    "url": "/Base-Apparel-coming-soon-page/static/js/main.7b37a487.chunk.js"
+  },
+  {
+    "revision": "9ffd9878bc521d4c603f",
+    "url": "/Base-Apparel-coming-soon-page/static/js/runtime-main.c9b10953.js"
+  },
+  {
+    "revision": "003b0c9744b45dea37e19234916edb43",
+    "url": "/Base-Apparel-coming-soon-page/static/media/hero-desktop.003b0c97.jpg"
+  },
+  {
+    "revision": "1d55799afb7254685302a38efea12fd1",
+    "url": "/Base-Apparel-coming-soon-page/static/media/hero-mobile.1d55799a.jpg"
+  },
+  {
+    "revision": "69950c2de382a8d4924260343e516fad",
+    "url": "/Base-Apparel-coming-soon-page/static/media/icon-arrow.69950c2d.svg"
+  },
+  {
+    "revision": "337809979308399afb36fa5f0e3fbcdf",
+    "url": "/Base-Apparel-coming-soon-page/static/media/icon-error.33780997.svg"
+  },
+  {
+    "revision": "877543ce69f4ed6f834f26e7dc4ca930",
+    "url": "/Base-Apparel-coming-soon-page/static/media/logo.877543ce.svg"
+  }
+]);
